41515d5a9b21dad42596be4ac35ff9ce619e2cf9
dong <dong@example.com>
2018-09-07 10:51:28 +0800

    step3

--- blah/data_types.py
+++ blah/data_types.py
@@ -2,6 +2,12 @@ class Symbol:
     def __init__(self, name):
         self.name = name
 
+    def __repr__(self):
+        return f"{type(self).__name__}('{self.name}')"
+
+    def __eq__(self, other):
+        return type(self) == type(other) and self.name == other.name
+
 class Integer:
     def __init__(self, string_value):
         self.string_value = string_value
@@ -17,3 +23,6 @@ class Integer:
 
     def __truediv__(self, other):
         return type(self)(str(int(self.string_value) // int(other.string_value)))
+
+    def __repr__(self):
+        return f"{type(self).__name__}('{self.string_value}')"
---
+++ blah/env.py
@@ -0,0 +1,29 @@
+class Env:
+    def __init__(self, outer):
+        self.outer = outer
+        self.data = {}
+
+    def set(self, key, value):
+        self.data[key] = value
+
+    def find(self, key):
+        if key in self.data:
+            return self #??
+
+        else:
+            if self.outer: #??
+                return self.outer.find(key)
+
+            else:
+                return None
+
+    def get(self, key):
+        environment = self.find(key)
+        if environment:
+            return environment.data[key]
+
+        else:
+            raise KeyError(f"'{key}' not found")
+
+    def __repr__(self):
+        return f"{type(self).__name__}({self.data}, outer={self.outer})"
